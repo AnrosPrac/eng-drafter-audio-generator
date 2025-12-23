cee8d499477674d13c4b97fc62b7670d87149d7a
AnrosPrac <sidhiqa20@gmail.com>
2025-12-23 20:47:29 +0530

    Docker version 3

--- main.py
+++ main.py
@@ -62,6 +62,6 @@ def health_check():
 
 if __name__ == "__main__":
     # Get port from environment variable for Render compatibility
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
